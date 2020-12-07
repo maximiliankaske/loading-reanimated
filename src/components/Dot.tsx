36990f0e020896aea721cd9319bf0dd3d0f2062b
Maximilian Kaske <maximilian@kaske.org>
2020-12-07 23:18:20 +0100

    :building_construction:

--- src/components/Dot.tsx
+++ src/components/Dot.tsx
@@ -6,6 +6,12 @@ import Animated, {
 import { toRad } from "react-native-redash";
 import { useDotsContext } from "./Dots";
 
+const inputRange2 = [0, 1];
+const inputRange3 = [0, 0.5, 1];
+const inputRange5 = [0, 0.25, 0.5, 0.75, 1];
+const defaultRange = [1, 0, 1];
+const zoomRange = [1, 1.4, 1];
+const flipRange = [0, 180];
 export interface DotStyles {
   size: number;
   backgroundColor: string;
@@ -18,52 +24,41 @@ export interface DotProps {
 const Dot = ({ index, styles }: DotProps) => {
   const { size, backgroundColor, spacing } = styles;
   const { progress, variant, numberOfDots } = useDotsContext();
-  const inputRange = [0, 0.25, 0.5, 0.75, 1];
   const upNDownRange = [0, spacing * 2, 0, -spacing * 2, 0];
-  const snakeRange = [
-    -(spacing + size) * 2,
-    -(spacing + size) * 1.5,
-    -(spacing + size),
-    -(spacing + size) * 0.5,
-    0,
-  ];
-  const opacityRange = [1, 0.5, 0, 0.5, 1];
-  const zoomRange = [1, 1.2, 1.4, 1.2, 1];
-  const frequencyRange = [size, size * 1.5, size * 2, size * 1.5, size];
-  const flipRange = [0, 45, 90, 135, 180];
+  const snakeRange = [-(spacing + size) * 2, 0];
+  const frequencyRange = [size, size * 2, size];
 
   const defaultStyle = useAnimatedStyle(() => ({
-    opacity: interpolate(progress.value, inputRange, opacityRange),
+    opacity: interpolate(progress.value, inputRange3, defaultRange),
   }));
 
   const upNDownStyle = useAnimatedStyle(() => ({
     transform: [
       {
-        translateY: interpolate(progress.value, inputRange, upNDownRange),
+        translateY: interpolate(progress.value, inputRange5, upNDownRange),
       },
     ],
   }));
 
   const snakeStyle = useAnimatedStyle(() => ({
-    opacity: interpolate(progress.value, inputRange, opacityRange),
     transform: [
       {
-        translateX: interpolate(progress.value, inputRange, snakeRange),
+        translateX: interpolate(progress.value, inputRange2, snakeRange),
       },
     ],
   }));
 
   const zoomStyle = useAnimatedStyle(() => ({
-    transform: [{ scale: interpolate(progress.value, inputRange, zoomRange) }],
+    transform: [{ scale: interpolate(progress.value, inputRange3, zoomRange) }],
   }));
 
   const frequencyStyle = useAnimatedStyle(() => ({
-    height: interpolate(progress.value, inputRange, frequencyRange),
+    height: interpolate(progress.value, inputRange3, frequencyRange),
   }));
 
   const flipStyle = useAnimatedStyle(() => ({
     transform: [
-      { rotateX: toRad(interpolate(progress.value, inputRange, flipRange)) },
+      { rotateX: toRad(interpolate(progress.value, inputRange2, flipRange)) },
     ],
   }));
 
